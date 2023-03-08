f98e0e904139ef6d8314d074b90f0ea3a2a7092d
Matthew Van Gelder <vangelder@gmail.com>
2023-03-08 12:23:50 -0800

    Added extension method to enable duckit for web apis using controllers

---
+++ gzbits.DuckIt.Mvc/Extensions/MvcBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using gzbits.DuckIt.Mvc.ResultExecutors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace gzbits.DuckIt.Mvc.Extensions
+{
+    public static class MvcBuilderExtensions
+    {
+        public static IMvcBuilder UseDuckIt(this IMvcBuilder mvcBuilder)
+        {
+            mvcBuilder?.Services?.AddSingleton<IActionResultExecutor<ObjectResult>, ProducesResponseTypeResultExecutor>();
+            return mvcBuilder;
+        }
+    }
+}
