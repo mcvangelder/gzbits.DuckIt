2a48f077c93b20211f6f4de724bbc3b4d10cb3cb
Matthew Van Gelder <vangelder@gmail.com>
2023-03-18 14:36:55 -0700

    Created schema and source objects in namespace "Objects" Added DuckItException, DuckItProxy, DuckItProxyGenerator, DynamicTypeBuilder

---
+++ gzbits.DuckIt/Proxy/DuckItProxyGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gzbits.DuckIt.Proxy
+{
+    internal interface IDuckItProxyGenerator
+    {
+        object? CreateProxyInstance(object source, Type type);
+    }
+
+    internal class DuckItProxyGenerator : IDuckItProxyGenerator
+    {
+        private DynamicTypeBuilder TypeBuilder { get; set; }
+
+        public DuckItProxyGenerator(DynamicTypeBuilder typeBuilder)
+        {
+            TypeBuilder = typeBuilder;
+        }
+
+        public object? CreateProxyInstance(object source, Type interfaceType)
+        { 
+            Type proxyType = TypeBuilder.GetOrCreateProxyType(source, interfaceType);
+            return Activator.CreateInstance(proxyType, source, this);
+        }
+    }
+}
