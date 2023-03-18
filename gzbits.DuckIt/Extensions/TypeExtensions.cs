a988803c69af98c0c582d09a4e095b50d8aea2f7
Matthew Van Gelder <vangelder@gmail.com>
2023-03-18 14:41:10 -0700

    Removed ObjectExtensions and related tests Removed Type extension method IsSimpleType

--- gzbits.DuckIt/Extensions/TypeExtensions.cs
+++ gzbits.DuckIt/Extensions/TypeExtensions.cs
@@ -13,16 +13,5 @@ namespace gzbits.DuckIt.Extensions
         {
             return type.GetProperties().Where(info => info.CanWrite).ToDictionary((prop) => prop.Name);
         }
-
-        /// <summary>
-        /// Determines if the type is a simple type: Enum, ValueType (int, double, float, long, DateTime, etc) or assignable to string
-        /// </summary>
-        /// <param name="type">The type to check if is primitive</param>
-        /// <returns>true if type represents an enum, value type, or assignable to string</returns>
-        internal static bool IsSimpleType(this Type type)
-        {
-            return type.IsEnum || type.IsValueType || type.IsAssignableTo(typeof(string));
-        }
-
     }
 }
