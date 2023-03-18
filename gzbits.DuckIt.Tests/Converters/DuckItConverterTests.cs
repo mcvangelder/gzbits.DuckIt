d3d9781a9dbac20a1293c86cbfdb8e6917cd29fa
Matthew Van Gelder <vangelder@gmail.com>
2023-03-18 14:40:32 -0700

    Updated DuckItConverter to use proxy generator instead of ObjectExtension.ToDynamic Updated IWeatherForecast Date datatype to match WeatherForecast Updated ProducesResponseTypeResultExectorTests

---
+++ gzbits.DuckIt.Tests/Converters/DuckItConverterTests.cs
@@ -0,0 +1,110 @@
+﻿using gzbits.DuckIt.Converters;
+using gzbits.DuckIt.Tests.Objects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace gzbits.DuckIt.Tests.Converters
+{
+    [TestClass]
+    public class DuckItConverterTests
+    {
+        private static IDuckItConverter converter = new DuckItConverter(null);
+
+        [TestClass]
+        public class SinglePropertyTests
+        {
+
+            [TestMethod]
+            public void IDuckItConverter_GenericConvert_SuccessfullyConverts()
+            {
+                Schemas.SingleProperty.ReadOnly.StringProperty? proxyInstance =
+                    converter.Convert<SourceObjects.SingleProperty.StringProperty, Schemas.SingleProperty.ReadOnly.StringProperty>(
+                        new SourceObjects.SingleProperty.StringProperty { Value = "test value" });
+
+                Assert.IsNotNull(proxyInstance);
+                Assert.AreEqual("test value", proxyInstance.Value);
+            }
+
+            [TestMethod]
+            public void DuckItConverter_Convert_SuccessfullyConverts()
+            {
+                var proxyInstance =
+                    converter.Convert(
+                        new SourceObjects.SingleProperty.StringProperty { Value = "test value" }, typeof(Schemas.SingleProperty.ReadOnly.StringProperty)) as Schemas.SingleProperty.ReadOnly.StringProperty;
+
+                Assert.IsNotNull(proxyInstance);
+                Assert.AreEqual("test value", proxyInstance?.Value);
+            }
+
+            [TestMethod]
+            public void DuckItConverter_GenericConvert_IntegerProperty_SuccessfullyConverts()
+            {
+                SourceObjects.SingleProperty.IntegerProperty objectWithIntegerProperty = new() { Value = 1 };
+
+                var proxyInstance = converter.Convert<SourceObjects.SingleProperty.IntegerProperty, Schemas.SingleProperty.ReadOnly.ValueTypeProperty.IntegerProperty>(objectWithIntegerProperty);
+
+                Assert.IsNotNull(proxyInstance);
+                Assert.AreEqual(1, proxyInstance?.Value);
+            }
+
+            [TestMethod]
+            public void DuckItConverter_GenericConvert_EnumerableStringProperty_SuccessfullyConverts()
+            {
+                string[] expectedValues = new[] { "value" };
+                SourceObjects.SingleProperty.EnumerableStringProperty objectWithEnumerableStrings = new SourceObjects.SingleProperty.EnumerableStringProperty { Value = expectedValues };
+
+                var proxyInstance = converter.Convert<SourceObjects.SingleProperty.EnumerableStringProperty, Schemas.SingleProperty.ReadOnly.EnumerableProperty.EnumerableStringProperty>(objectWithEnumerableStrings);
+
+                Assert.IsNotNull(proxyInstance);
+                Assert.AreEqual(expectedValues.Length, proxyInstance?.Value?.Length);
+                Assert.AreEqual(expectedValues[0], proxyInstance?.Value?[0]);
+
+            }
+        }
+
+        [TestClass]
+        public class MultiPropertyTests
+        {
+            [TestMethod]
+            public void DuckItConverter_GenericConvert_IntAndStringProperties_SuccessfullyConverts()
+            {
+                SourceObjects.MultiProperty.TwoProperties_Int_String objectWithIntAndStringProperty = new() { StringValue = "value", IntValue = 1 };
+
+                var proxyInstance = converter.Convert<SourceObjects.MultiProperty.TwoProperties_Int_String, Schemas.MultiProperty.ReadOnly.TwoProperties_Int_String>(objectWithIntAndStringProperty);
+
+                Assert.IsNotNull(proxyInstance);
+                Assert.AreEqual(1, proxyInstance.IntValue);
+                Assert.AreEqual("value", proxyInstance.StringValue);
+            }
+        }
+
+        [TestClass]
+        public class SupportedTypesTests
+        {
+            [TestMethod]
+            public void DuckItConverter_Convert_InAndOutTypesAreSame_ReturnsSameInstance()
+            {
+                // The actual type and instance values are irrelevant, this is test is only concerned with whether the types are identical
+                SourceObjects.SingleProperty.StringProperty source = new SourceObjects.SingleProperty.StringProperty { Value = "value" }; 
+                Type outType = typeof(SourceObjects.SingleProperty.StringProperty);
+
+                var instance = converter.Convert(source, outType);
+                Assert.IsNotNull(instance);
+                Assert.AreSame(source, instance);
+
+                instance = converter.Convert<SourceObjects.SingleProperty.StringProperty, SourceObjects.SingleProperty.StringProperty>(source);
+                Assert.IsNotNull(instance);
+                Assert.AreSame(source, instance);
+            }
+
+            [TestMethod]
+            public void DuckItConverter_Convert_OutputType_NotSameAndIsNotInterface_ThrowsDucktItException()
+            {
+                SourceObjects.SingleProperty.IntegerProperty source = new SourceObjects.SingleProperty.IntegerProperty {  Value = 1 };
+                Type outType = typeof(SourceObjects.SingleProperty.StringProperty);
+
+                DuckItException exception = Assert.ThrowsException<DuckItException>(() => converter.Convert(source, outType));
+                Assert.IsInstanceOfType(exception.InnerException, typeof(ArgumentException));
+            }
+        }
+    }
+}
--- gzbits.DuckIt/Converters/DuckItConverter.cs
+++ gzbits.DuckIt/Converters/DuckItConverter.cs
@@ -1,7 +1,9 @@
 ﻿using gzbits.DuckIt.Extensions;
+using gzbits.DuckIt.Proxy;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,29 +12,33 @@ namespace gzbits.DuckIt.Converters
 {
     public interface IDuckItConverter
     {
-        dynamic Convert<Tout, Tin>(Tin source)
+        Tout? Convert<Tin, Tout>(Tin source)
             where Tout : class
-            where Tin : notnull
+            where Tin : class
         {
             return Convert(source, typeof(Tout));
         }
 
-        dynamic Convert<Tin>(Tin source, Type outType) where Tin : notnull;
+        dynamic? Convert<Tin>(Tin source, Type outType) where Tin : class;
     }
 
     public class DuckItConverter : IDuckItConverter
     {
         private readonly ILogger<DuckItConverter>? logger;
+        private readonly IDuckItProxyGenerator duckItProxyGenerator = new DuckItProxyGenerator(new DynamicTypeBuilder());
 
-        public DuckItConverter(ILogger<DuckItConverter> logger)
+        public DuckItConverter(ILogger<DuckItConverter>? logger)
         {
             this.logger = logger;
         }
 
-        public dynamic Convert<Tin>(Tin source, Type outType) where Tin : notnull
+        public object? Convert<Tin>(Tin source, Type outType) where Tin : class
         {
+            if(source.GetType() == outType) { return source; }
+            if (!outType.IsInterface) { throw new DuckItException($"Provided type must be an interface. See inner exception for details.", new ArgumentException($"Not an interface: {outType}.", nameof(outType))); }
+
             logger?.LogDebug($"Converting {typeof(Tin)} to {outType.GetType()}");
-            return source.ToDynamic(outType);
+            return duckItProxyGenerator.CreateProxyInstance(source, outType);
         }
     }
 }
